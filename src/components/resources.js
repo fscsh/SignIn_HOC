44a7d1a5f1a4cbdd5b82a6269d3c2c4e66110e81
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-04-14 13:00:11 -0700

    finish

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+
+export default()=> {
+    return(
+        <div>
+            HAHAHAHAHAHA
+            <ul>
+                <li> apple </li>
+                <li> apple </li>
+                <li> apple </li>
+            </ul>
+        </div>
+    )
+}
